bdb1ba30c1131cdab36bb669a0b0ee6ded7c216c
Madhara Madhubhashini <surammyajith5@gmail.com>
2025-02-24 05:21:52 -0800

    Update in ITBIN-2211-0327

--- app/src/main/java/com/example/carpark/MainActivity2.java
+++ app/src/main/java/com/example/carpark/MainActivity2.java
@@ -2,6 +2,7 @@ package com.example.carpark;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,6 +22,7 @@ public class MainActivity2 extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
 
+
         help = findViewById(R.id.imageView9);
         help.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +35,35 @@ public class MainActivity2 extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Button btnMap = findViewById(R.id.button2);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the booking activity
+                Intent intent = new Intent(MainActivity2.this, Map.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnAbs = findViewById(R.id.button5);
+        btnAbs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the booking activity
+                Intent intent = new Intent(MainActivity2.this, About_Us.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView btnProfile = findViewById(R.id.imageView5);
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the booking activity
+                Intent intent = new Intent(MainActivity2.this, Profile.class);
+                startActivity(intent);
+            }
         });
     }
 }
